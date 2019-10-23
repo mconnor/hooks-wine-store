4948b30ebf8d11a07c504b9cb50b3ae6a0c6cb26
mike connor <mike@rocketnumber9.org>
2019-10-23 17:47:38 -0500

    flow - custom hook is working
    but how to generalize it?

--- src/hooks/useGetAverage.js
+++ src/hooks/useGetAverage.js
@@ -2,28 +2,38 @@
 
 import * as React from "react";
 
- function useGetAverage(ratingArray: Array<{stars: number}>) {
+type Props = {
+    stars: number
+};
 
-    const [average, setAverage] = React.useState(-1)
+function useGetAverage(ratingArray: $ReadOnlyArray<Props>) {
+  const [value, setValue] = React.useState();
+  //console.log('xxx', Object.keys(ratingArray[0]))   //stars
+  
+  React.useEffect(() => {
+    function getAverage(arr: $ReadOnlyArray<Props>):void {
+        
+      const len = arr.length;
+      if (len === 0) return;
+      
+      //const _key: string = Object.keys(arr[0]);
 
-    React.useEffect(() => {
-        function getAverage(arr: Array<{'stars': number}>){
-            const len = arr.length;
-            if (len === 0) {
-              return -1;
-            }
-            let initialValue = 0;
-            const total = arr
-              .map(item => item.stars)
-              .reduce((totalStars, curr) => totalStars + curr, initialValue);
-              setAverage(total / len);
-              console.log('avg', total / len)
-        }
-        getAverage(ratingArray)
+    //   const firstRating:Props = arr[0];
+    //   const _key: stars = Object.keys(firstRating)
 
-    })
-    return  average;
+      const total = arr
+        .map(item => item.stars)
+        .reduce((runningTotal, curr) => runningTotal + curr, 0);
 
+      setValue(total / len);
+    }
+    getAverage(ratingArray);
+    //console.log('React.useEffect ', ratingArray[0])  
+    
+  }, [ratingArray]);
+
+  //return [value, setValue];
+  return [value, setValue];
 }
 
 export default useGetAverage;
