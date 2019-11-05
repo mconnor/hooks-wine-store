e2696fd2d0bff5f5be311c066acfd83eb5f46ab0
mike connor <mike@rocketnumber9.org>
2019-11-05 13:36:33 -0600

    wine.test.js

--- src/components/Wine/__tests__/Wine.test.js
+++ src/components/Wine/__tests__/Wine.test.js
@@ -1,42 +1,61 @@
 import React from 'react';
-import { render, cleanup, waitForElement, getByTestId } from '@testing-library/react';
+import { render, cleanup, getByTestId } from '@testing-library/react';
 
 import Wine from '../Wine';
 
-// global.fetch = require('jest-fetch-mock');
 
-console.error = jest.fn();
+afterEach(() => {
+    cleanup();
+    console.error.mockClear();
+  });
 
-const wineProps = 
-{
+  console.error = jest.fn();
+
+test('<Wine />', () => {
+    render(<Wine />);
+    expect(console.error).not.toHaveBeenCalled();
+  });
+
+
+
+const wineProps = {
     "id": "f2ca57a5-d9da-4808-9164-8d6e0da0aef5",
     "name": "Apothic Red",
-    "vintage": "2015",
+    "vintage": 2015,
     "vineyard": "Apothic",
     "type": "Red Blend",
-    region: "California",
+    "region": "California",
     "unitsSold": 51,
     "ratings": [{
-      "stars": 5
+        "stars": 5
     }, {
-      "stars": 3
+        "stars": 3
     }]
-  }
-  ;
+};
 
-  test('<Wine /> with wine props', async () => {
-    const { debug, getByTestId, container } = render(
+test('<Wine /> with wine props', () => {
+    const { debug,getByTestId } = render(
         <Wine 
-            name={`${wineProps.name}`} 
-            vintage={`${wineProps.vintage}`}
-            region={`${wineProps.region}`}/>
-        );
-   expect(getByTestId('name-container').textContent).toBe(wineProps.name);
-   expect(getByTestId('vintage-container').textContent).toBe(wineProps.vintage);
+            name = {wineProps.name}
+            vintage = {wineProps.vintage}
+            region = {
+                `${wineProps.region}`
+            }
+            vineyard = {
+                `${wineProps.vineyard}`
+            }
+            type = {
+                `${wineProps.type}`
+            }
+        />
+    );
+    expect(getByTestId('name-container').textContent).toBe(wineProps.name);
+    expect(getByTestId('vintage-container').textContent).toBe(wineProps.vintage.toString());
 
-   expect(getByTestId('region-container').textContent).toBe(wineProps.region);
-   
-   // expect(container.firstChild).toMatchSnapshot()
+    expect(getByTestId('region-container').textContent).toBe(wineProps.region);
+    expect(getByTestId('vineyard-container').textContent).toBe(wineProps.vineyard);
+    expect(getByTestId('type-container').textContent).toBe(wineProps.type);
+
     debug();
 
-  });
+});
