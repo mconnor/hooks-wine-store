9f8818f01057748c51188cbb4d73db96a44fac6c
mike connor <mike@rocketnumber9.org>
2019-12-12 12:49:22 -0600

    useTrails

--- src/components/ListContainer/ListContainer.js
+++ src/components/ListContainer/ListContainer.js
@@ -1,68 +1,96 @@
 // @flow
 
-import * as React from "react";
-import Wine from "../Wine/Wine";
-import styles from "./ListContainer.module.css";
+import * as React from 'react';
+import { useTrail } from 'react-spring';
+import Wine from '../Wine/Wine';
+import styles from './ListContainer.module.css';
 
 type RatingProp = {
-  stars: number
+	stars: number
 };
 
 type WineProps = {
-  id: string,
-  name: string,
-  vintage: number,
-  vineyard: string,
-  type: string,
-  region: string,
-  unitsSold: number,
-  highSeller: boolean,
-  onLearnMore: Function,
-  ratings: $ReadOnlyArray<RatingProp>
+	id: string,
+	name: string,
+	vintage: number,
+	vineyard: string,
+	type: string,
+	region: string,
+	unitsSold: number,
+	highSeller: boolean,
+	onLearnMore: Function,
+	ratings: $ReadOnlyArray<RatingProp>
 };
 type Props = {
-  wines: $ReadOnlyArray<WineProps>,
-  onLearnMore: Function,
-  showingAllWines: boolean
+	wines: $ReadOnlyArray<WineProps>,
+	onLearnMore: Function,
+	showingAllWines: boolean
 };
 
 function ListContainer(props: Props) {
-  const [mostSold, setMostSold] = React.useState();
-  const { wines, onLearnMore, showingAllWines } = { ...props };
-  
-  
+	const [ mostSold, setMostSold ] = React.useState();
+	const { wines, onLearnMore, showingAllWines } = { ...props };
 
-  const findMostUnitsSold = (arr: $ReadOnlyArray<WineProps>): number => {
-    const maxCallback = (acc, cur) => Math.max(acc, cur);
-    return arr.map(wine => wine.unitsSold).reduce(maxCallback);
-  };
+	const [ trail, set ] = useTrail(wines.length, () => ({
+		opacity: 0,
+		transform: 'scale(0.5)'
+		// config:  { mass: 10, tension: 200, friction: 50 }
+	}));
 
+	const findMostUnitsSold = (arr: $ReadOnlyArray<WineProps>): number => {
+		const maxCallback = (acc, cur) => Math.max(acc, cur);
+		return arr.map((wine) => wine.unitsSold).reduce(maxCallback);
+	};
 
-  React.useEffect(() => {
-    setMostSold(findMostUnitsSold(wines));
-  }, [wines]);
+	React.useEffect(
+		() => {
+			setMostSold(findMostUnitsSold(wines));
+		},
+		[ wines ]
+	);
 
-  return (
-    <section>
-      {wines.map((wine, index) => (
-        <Wine
-        fadeDelay={index*500}
-          key={wine.id}
-          id={wine.id}
-          name={wine.name}
-          vintage={wine.vintage}
-          vineyard={wine.vineyard}
-          type={wine.type}
-          region={wine.region}
-          unitsSold={wine.unitsSold}
-          ratings={wine.ratings}
-          highSeller={wine.unitsSold === mostSold}
-          onLearnMore={onLearnMore}
-          showingAllWines={showingAllWines}
-        />
-      ))}
-    </section>
-  );
+	set({
+		opacity: 1,
+		transform: 'scale(1)'
+	});
+	return (
+		<div className={styles.container}>
+			{trail.map((animation, index) => (
+				<Wine
+					animStyle={animation}
+					id={wines[index].id}
+					name={wines[index].name}
+					vintage={wines[index].vintage}
+					vineyard={wines[index].vineyard}
+					type={wines[index].type}
+					region={wines[index].region}
+					unitsSold={wines[index].unitsSold}
+					ratings={wines[index].ratings}
+					highSeller={wines[index].unitsSold === mostSold}
+					onLearnMore={onLearnMore}
+					showingAllWines={showingAllWines}
+				/>
+			))}
+		</div>
+	);
 }
 
 export default ListContainer;
+
+/* 
+<animated.div key={key} style={props} />
+				<Wine
+					key={item.id}
+					id={item.id}
+					name={item.name}
+					vintage={item.vintage}
+					vineyard={item.vineyard}
+					type={item.type}
+					region={item.region}
+					unitsSold={item.unitsSold}
+					ratings={item.ratings}
+					highSeller={item.unitsSold === mostSold}
+					onLearnMore={onLearnMore}
+					showingAllWines={showingAllWines}
+				/>
+            </animated.div> */
