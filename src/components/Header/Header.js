d5a2384e7d17a6c5982eb9419bf90d6927ea970d
mike connor <mike@rocketnumber9.org>
2019-12-12 13:37:09 -0600

    animation header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,29 @@
 // @flow
 
-import * as React from "react";
-import styles from './Header.module.css'
-
+import * as React from 'react';
+import { useSpring, animated } from 'react-spring';
+import styles from './Header.module.css';
 
 type Props = {
-    name: string,
-}
-
+	name: string
+};
 
 function Header(props: Props) {
-    return (
-       
-            <h1 data-testid="title-container" className={styles.headline}>{props.name}</h1>
-       
-    )
+	const slide = useSpring({
+        transform: `translate3d(0, 0, 0)`,
+		from: {
+            transform: `translate3d(1000px, 0, 0)`
+		}
+    });
+    
+
+	return (
+	
+			<h1 data-testid="title-container" className={styles.headline}>
+				{props.name}
+			</h1>
+
+	);
 }
 
-export default  Header;
+export default Header;
