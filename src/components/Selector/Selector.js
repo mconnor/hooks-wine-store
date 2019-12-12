25ea5f0172cfd0eb0a6504a6825e0e5759c2a54c
mike connor <mike@rocketnumber9.org>
2019-12-12 13:36:44 -0600

    style - selector

--- src/components/Selector/Selector.js
+++ src/components/Selector/Selector.js
@@ -3,6 +3,7 @@
 import * as React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import SplitButton from "react-bootstrap/SplitButton";
+import styles from './Selector.module.css';
 
 type RatingProp = {
     stars: number
@@ -38,29 +39,32 @@ function Selector(props: Props) {
   const selectVintage = (eventKey: number) => {
     const _year = parseInt(eventKey, 10);
     setChosenVintage(_year);
-    console.log(_year);
     filterByVintage(_year);
   };
 
   return (
-    <SplitButton
-      key="vintage"
-      title="Select a Year"
-      onSelect={selectVintage}
-      size="sm"
-      drop="right"
-    >
-      {chosenVintage ? (
-        <Dropdown.Item onSelect={selectVintage} eventKey={0} key={0}  as="button">
-          <span>All Vintages</span>
-        </Dropdown.Item>
-      ) : null}
-      {uniqueYears.map(year => (
-        <Dropdown.Item onSelect={selectVintage} eventKey={year} key={year}  as="button">
-          <span>{year}</span>
-        </Dropdown.Item>
-      ))}
-    </SplitButton>
+      <div className="selector">
+        <SplitButton
+        key="vintage"
+        title="Select a Year"
+        onSelect={selectVintage}
+        size="sm"
+        drop="right"
+        >
+        {chosenVintage ? (
+            <Dropdown.Item onSelect={selectVintage} eventKey={0} key={0}  as="button"
+                data-testid="dropdown">
+            <span>All Vintages</span>
+            </Dropdown.Item>
+        ) : null}
+        {uniqueYears.map(year => (
+            <Dropdown.Item onSelect={selectVintage} eventKey={year} key={year}  as="button"
+                data-testid="dropdown">
+            <span data-testid="year-container">{year}</span>
+            </Dropdown.Item>
+        ))}
+        </SplitButton>
+    </div>  
   );
 }
 
